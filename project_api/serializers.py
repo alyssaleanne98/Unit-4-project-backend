25e5c45bbb1375de900632371dac94d1a98e9857
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-02-18 02:04:22 -0800

    index route complete

--- project_api/serializers.py
+++ project_api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Cards
 
-class CardsSerializers(serializers.ModelSerializers):
+class CardsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Cards
         fields = '__all__'
--- project_api/views.py
+++ project_api/views.py
@@ -3,13 +3,30 @@ from rest_framework.views import APIView #as super to your class
 from rest_framework.response import Response #to send data to frontend
 from rest_framework import status # to include status codes in your response
 from .models import Cards
-
+from rest_framework import status #to include status codes in your response
+from .serializers import CardsSerializers #to format data to and from the database, enforces schema
 
 # Create your views here.
 
+# class Card
+#  GET     /card - index
+#  POST    /card - create
+
+# class  (CardDetail) - use primary key (pk) as argument to access id
+
+#  GET     /card/:id - show
+#  PUT     /card/:id - update
+#  DELETE  /card/:id - delete
 
 class Card(APIView):
-    pass
+    def get(self, request):
+        #Index request
+        print(request)
+        #Get all cards from card table
+        card = Cards.objects.all()
+        #Use serializer to format table data to JSON
+        data = CardsSerializers(card, many=True).data
+        return Response(data)
 
 class CardDetail(APIView):
     pass
